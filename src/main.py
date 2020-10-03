beb8edd963ebd9ce36e095a1b02d2a856fd6f020
Flandy2010 <979546782@qq.com>
2020-10-03 17:02:16 +0800

    增加了argparse命令行

--- src/main.py
+++ src/main.py
@@ -1,10 +1,18 @@
 import os
-from page_scrapy import get_page
+import argparse
+from spider import Spider
 from datahelper import DataHelper
 
 output_keywords = [["title", "标题"], ["keywords", "关键词"], ["time", "日期"], ["section", "分类"], ["content", "内容"], ["url", "网址"]]
 
 if __name__=="__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--more_news_times", type=int, default=3, help="Number of click the 'more news' button")
+    parser.add_argument('-o', "--output_dir", type=str, default="../output")
+    parser.add_argument('-v', "--visited_url", type=str, default="../input/visited_url.txt")
+    parser.add_argument('-r', "--root_url", type=str, default="../input/root_url.txt")
+    args = parser.parse_args()
+
     datahelper = DataHelper()
-    data = get_page("https://gogo.mn/r/y6vg2")
-    datahelper.output_dict(data, "./output/test.txt", output_keywords)
+    spider = Spider(datahelper=datahelper, args=args)
+    spider.start()
