46112399aac9ce5850ee8f9c70900dc0b36b59a8
Flandy2010 <979546782@qq.com>
2020-10-08 18:16:19 +0800

    增加了部分参数

--- src/main.py
+++ src/main.py
@@ -7,9 +7,12 @@ output_keywords = [["title", "标题"], ["keywords", "关键词"], ["time", "日
 
 if __name__=="__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--more_news_times", type=int, default=3, help="Number of click the 'more news' button")
+    parser.add_argument("--more_news_times", type=int, default=1, help="Number of click the 'more news' button")
+    parser.add_argument("--threading_num", type=int, default=3, help="Number of threading")
     parser.add_argument('-o', "--output_dir", type=str, default="../output")
+    parser.add_argument('-i', "--ip_list_file", type=str, default="../input/alive_ip_list.txt")
     parser.add_argument('-v', "--visited_url", type=str, default="../input/visited_url.txt")
+    parser.add_argument('-u', "--unvisited_url", type=str, default="../input/unvisited_url.txt")
     parser.add_argument('-r', "--root_url", type=str, default="../input/root_url.txt")
     args = parser.parse_args()
 
